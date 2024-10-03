3646d07aac1ebc758c2dc1ee26f8b87f2244d4c5
Carlos Ramos <carlos.dg147@gmail.com>
2024-10-03 16:51:26 -0400

    Fix register and validation form register.

--- src/main/java/com/university/demo/controllers/AuthController.java
+++ src/main/java/com/university/demo/controllers/AuthController.java
@@ -3,11 +3,14 @@ package com.university.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.university.demo.models.User;
 import com.university.demo.services.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class AuthController {
@@ -22,7 +25,11 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String registerUser(User user) {
+    public String registerUser(@Valid @ModelAttribute User user, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            return "register";
+        }
+        
         userService.saveUser(user);
         return "redirect:/login";
     }
--- src/main/java/com/university/demo/models/User.java
+++ src/main/java/com/university/demo/models/User.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity(name = "usuarios")
 public class User {
@@ -14,14 +16,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50)
     @NotNull
     @Column(unique = true)
     private String username;
-
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 4, max = 100)
     @NotNull
     private String password;
 
-    @NotNull
     private String role;
 
     public Long getId() {
